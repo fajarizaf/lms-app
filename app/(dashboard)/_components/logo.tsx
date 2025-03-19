3287670bb0d8c525a601fe77249c8838847305eb
Fajar Riza Fauzi <fajarizaf@gmail.com>
2025-03-19 11:57:22 +0700

    change logo

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -3,10 +3,10 @@ import Image from "next/image"
 export const Logo = () => {
     return ( 
         <Image 
-        height={130}
-        width={130}
+        height={128}
+        width={128}
         alt="logo svg"
-        src = "/logo.svg"
+        src = "/tutor-lms.webp"
         />
      );
 }
